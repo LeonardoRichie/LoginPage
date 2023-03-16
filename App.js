7f71fde969df37e330d990ffe3b49ef1cee90d31
LeonardoRichie <91533882+LeonardoRichie@users.noreply.github.com>
2023-03-16 23:54:33 +0700

    t
    t

---
+++ App.js
@@ -0,0 +1,21 @@
+import "./TodoApp/App.css";
+import Title from "./TodoApp/components/Title";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Reset from "./Reset";
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Title />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+export default App;
